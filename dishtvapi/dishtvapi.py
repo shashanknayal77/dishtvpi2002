03f8bdae5b568513d37164fc2f5c794f510f1304
shashanknayal77 <134627759+shashanknayal77@users.noreply.github.com>
2024-05-16 14:39:07 +0530

    Update dishtvapi.py

--- dishtvapi/dishtvapi.py
+++ dishtvapi/dishtvapi.py
@@ -87,7 +87,7 @@ def load_config():
             "redirect_uris": (
                 ["http://localhost:8501"]
                 if IS_LOCAL
-                else ["https://share.streamlit.io/shashanknayal77/dishtvpi2002"]
+                else ["https://share.streamlit.io/shashanknayal77/dishtvpi2002/dishtvapi/dishtvapi.py"]
             ),
         }
     }
